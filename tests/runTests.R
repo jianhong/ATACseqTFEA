6ae03dde27098ea08eb15dea98732b7520713a6a
JIANHONG OU <jianhong@users.noreply.github.com>
2022-05-13 11:26:00 -0400

    add test unit.

--- tests/runTests.R
+++ tests/runTests.R
@@ -1,9 +1,5 @@
 require("ATACseqTFEA") || stop("unable to load Package:ATACseqTFEA")
-require("GenomicRanges") || stop("unable to load Package::GenomicRanges")
-require("GenomicAlignments") || stop("unalbe to load Package:GenomicAlignments")
-require("utils") || stop("unable to load Package:utils")
 require("BSgenome.Drerio.UCSC.danRer10") ||
   stop("unable to load Package:BSgenome.Drerio.UCSC.danRer10")
-require("Rsamtools") || stop("unable to load Rsamtools")
 require("testthat") || stop("unable to load testthat")
 test_check("ATACseqTFEA")
--- tests/testthat/test_prepareBindingSites.R
+++ tests/testthat/test_prepareBindingSites.R
@@ -1,3 +1,13 @@
 test_that("prepareBindingsites works not correct", {
-
+  motifs <- readRDS(system.file("extdata", "PWMatrixList.rds",
+                                package="ATACseqTFEA"))
+  seqlev <- "chr1"
+  mts <- prepareBindingSites(motifs, Drerio, seqlev="chr1",
+                             grange=GRanges("chr1",
+                                            IRanges(5000, 100000)),
+                             p.cutoff = 5e-05)
+  res <- readRDS((system.file("extdata", "bindingSites.rds",
+                              package="ATACseqTFEA")))
+  expect_true(all(start(res)==start(mts)))
+  expect_true(all(end(res)==end(mts)))
 })
