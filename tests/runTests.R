89a8d8602e04352c37228245fed368d3c6b32fee
JIANHONG OU <jianhong@users.noreply.github.com>
2022-11-11 09:20:46 -0500

    clean the action file.

--- tests/runTests.R
+++
@@ -1,9 +0,0 @@
-require("ATACseqTFEA") || stop("unable to load Package:ATACseqTFEA")
-require("BSgenome.Drerio.UCSC.danRer10") ||
-  stop("unable to load Package:BSgenome.Drerio.UCSC.danRer10")
-require("SummarizedExperiment") ||
-  stop("unable to load Package:SummarizedExperiment")
-require("GenomicRanges") ||
-  stop("unable to load Package:GenomicRanges")
-require("testthat") || stop("unable to load testthat")
-test_check("ATACseqTFEA")
